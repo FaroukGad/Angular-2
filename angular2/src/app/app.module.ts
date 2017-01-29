cecf26685a73b2894cac57d34fcd6f9df000a324
Margauw <margaux.dubezin@devinci.fr>
2017-01-29 18:18:44 +0100

    route

--- angular2/src/app/app.module.ts
+++ angular2/src/app/app.module.ts
@@ -2,12 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-<<<<<<< HEAD
-import { RouterModule, Routes } from '@angular/router'
-=======
 import { RouterModule } from '@angular/router';
 import { ROUTES } from './app.routes';
->>>>>>> 3e8ee671d22bb02fc35009515121de7029e95bf9
 
 import { AppComponent } from './app.component';
 import {JeuService} from './jeu.service';
